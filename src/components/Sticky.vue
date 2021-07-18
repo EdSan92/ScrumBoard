91a48911904f5121b4d06cd9cafd1a42ebf05213
Edgar Sanroesdi <esanroesdi@vitas.nl>
2021-07-18 13:29:18 +0200

    Reuse color invertor function

--- src/components/Sticky.vue
+++ src/components/Sticky.vue
@@ -42,6 +42,7 @@
 <script lang="ts">
 import { defineComponent, ref } from "vue";
 import StickyForm from "@/components/StickyForm.vue";
+import invertColor from "@/composables/useInvertColor";
 
 export default defineComponent({
   name: "Sticky",
@@ -60,30 +61,6 @@ export default defineComponent({
     }
     function updateSticky(id: string) {
       emit("updateSticky", id);
-    }
-    function invertColor(hex: string) {
-      if (hex.indexOf("#") === 0) {
-        hex = hex.slice(1);
-      }
-      // convert 3-digit hex to 6-digits.
-      if (hex.length === 3) {
-        hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
-      }
-      if (hex.length !== 6) {
-        throw new Error("Invalid HEX color.");
-      }
-      // invert color components
-      var r = (255 - parseInt(hex.slice(0, 2), 16)).toString(16),
-        g = (255 - parseInt(hex.slice(2, 4), 16)).toString(16),
-        b = (255 - parseInt(hex.slice(4, 6), 16)).toString(16);
-      // pad each with zeros and return
-      return "#" + padZero(r) + padZero(g) + padZero(b);
-    }
-
-    function padZero(str: string, len?: number) {
-      len = len || 2;
-      var zeros = new Array(len).join("0");
-      return (zeros + str).slice(-len);
     }
     return {
       saveSticky,
